d916e4c40ad6e158220d297a000db05d9a7352f0
test <test@qq.com>
2022-05-16 22:24:53 +0800

    登录页样式调整方法

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,8 @@ module.exports = {
     ['import', {
       libraryName: 'vant',
       libraryDirectory: 'es',
-      style: true
+      // style: true 
+      style: (name) => `${name}/style/less`  //指定样式路径 style: (name) 就会换成这个路径${name}/style/less
     }, 'vant']
   ]
 }
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,28 @@
 <template>
     <div>
-         <van-nav-bar title="仿头条"/>  <!--配置了路由要设置挂在点 -->
+         <van-nav-bar title="仿头条-登录"/>  <!--配置了路由要设置挂在点 -->
     </div>
 </template>
 
 <script>
 export default{
-
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+/* 此类名是van-nav-bar组件内根标签 */
+//   .van-nav-bar {
+//    background-color: #007bff;
+//   }
+//  此选择器名字是van-nav-bar组件内标签
+//  scoped尝试把此选择器后属性选择器匹配当前页面标签 (选不中组件内部的) 
+//  lang="less" 当前style标签内是less语法
+//  用/deep/就不会被vscode标记红线
+//  /deep/ 会把属性选择器加到前面 (用后代选择器的方式往里找匹配的类名)
+//  结论: 要修改组件内样式, 如果你用了scoped, 就需要在选择器前/deep/即可 
+ /deep/ .van-nav-bar__title {
+   color: white; //这里css优先级要比cover.less高
+ }
+
+
+</style>
