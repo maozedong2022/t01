7ae71c8fd4290cbe406b957e75682c57f15b3bbd
test <test@qq.com>
2022-05-17 15:49:33 +0800

    首页初始化+底部标签页

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import 'amfe-flexible' //适配移动端字体大小
 
 import {NavBar} from 'vant'
 
-import {Form, Field, Button } from 'vant'  //引入表单组件
+import {Form, Field, Button,Tabbar,TabbarItem } from 'vant'  //引入表单组件
 Vue.use(Form)
 Vue.use(Field)
 Vue.use(Button)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
 
 
 
